bacf8d9d75171bfe25092f9af4ca4a1ded5e9f3d
RasikaJade1 <jaderasika4@gmail.com>
2025-09-02 23:29:19 +0530

    Changes in server.js

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,6 +1,11 @@
 import express from "express";
-import { checkAuth, login, signup, updateProfile } from "../controllers/userController.js";
-import { protectRoute } from "../middlewares/auth.js";
+import {
+  checkAuth,
+  login,
+  signup,
+  updateProfile,
+} from "../controllers/userController.js";
+import { protectRoute } from "../middleware/auth.js";
 
 const userRouter = express.Router();
 
